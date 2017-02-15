dce9bf1767982da083e870db92e33f6987cbb020
shiva.ping@gmail.com <Moukhthi9>
2017-02-15 15:02:36 +0530

    addback

---
+++ KeyPair.PairActor/BackUp/Interface/IBackupStore.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace KeyPair.PairActor.BackUp.Interface
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.ServiceFabric.Data;
+
+    public interface IBackupStore
+    {
+        long backupFrequencyInSeconds { get; }
+
+        Task ArchiveBackupAsync(BackupInfo backupInfo, CancellationToken cancellationToken);
+
+        Task<string> RestoreLatestBackupToTempLocation(CancellationToken cancellationToken);
+
+        Task DeleteBackupsAsync(CancellationToken cancellationToken);
+    }
+}
